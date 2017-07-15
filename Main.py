e214504578c5c44db5fc39ce73c6cf2ed50528c6
Eric Mier <ericjam@umich.edu>
2017-07-14 23:28:27 -0700

    Making some good progress on some of the screens for the app including the create tournament screen which should be done at this point. Now working on the screen for loading a tournament. Next will be the screen for creating the bracket, then filling it out. Lots of good stuff on the way!

--- Main.py
+++ Main.py
@@ -33,23 +33,19 @@ class App(tk.Tk):
 
         self.config(menu=menubar)
 
-        self.frames = {}
-        '''
-        for F in (StartPage, PageOne, PageTwo):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
+        self.brackets = {}
 
-            # Set frames to take up entire window
-            frame.grid(row=0, column=0, sticky="nsew")
-        '''
-        self.frame = Bracket(parent=container, controller=self, cols=5, type="entry")
-        self.frame.grid(row=0, column=0, sticky="nsew")
-        self.frame.tkraise()
+        self.home = Home(parent=container, controller=self)
+        self.home.grid(row=0, column=0, sticky="nsew")
+        self.home.tkraise()
 
-#        self.frame1 = Bracket(parent=container, controller=self, cols=5)
- #       self.frame1.grid(row=0, column=0, sticky="nsew")
-  #      self.frame1.tkraise()
+
+       # self.frames = {}
+    #    dummy = StringVar()
+   #     self.entries = [dummy] * 17
+  #      self.frame = Bracket(parent=container, controller=self, cols=4, type="entry")
+ #       self.frame.grid(row=0, column=0, sticky="nsew")
+#        self.frame.tkraise()
 
     def show_frame(self, page_name):
         #Show a frame for the given page name
@@ -65,6 +61,7 @@ class Bracket(tk.Frame):
     def __init__(self, parent, controller, cols, type):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.parent = parent
 
         rows = 2 ** (cols - 1)
         self.scrollbar = Scrollbar(self)
@@ -79,8 +76,7 @@ class Bracket(tk.Frame):
 
         sum = 0
         self.labels = [""] * rows * 2
-        self.entries = []
-        self.entities = []
+        self.ind = 9
         for c in range(cols):
             prev = sum
             if c != 0:
@@ -99,10 +95,10 @@ class Bracket(tk.Frame):
                         self.entity = StringVar()
                         self.entry = Entry(self.canvas, textvariable=self.entity)
                         self.entry.pack()
-                        self.entities.append(self.entity)
+                        self.controller.entries[self.ind] = self.entity
+                        self.ind += 1
                         self.canvas.create_window(80 + (c * 140), 25 + (30 * r) + 15 * (sum),
                                                   anchor=S, window=self.entry)
-                        self.entries.append(self.entry)
 
                 if (r % 2 ** (c + 1) == 0) and (c < cols - 1):
                     self.canvas.create_line(10 + (140 * (c + 1)), 30 + (30 * r) + 15 * (sum), 10 + (140 * (c + 1)),
@@ -117,7 +113,80 @@ class Bracket(tk.Frame):
         self.canvas.config(scrollregion=self.canvas.bbox("all"))
 
     def submit_entries(self, event):
-        print self.entities[3].get()
+        for i in range(len(self.controller.entries)):
+            if self.controller.entries[i].get():
+                print self.controller.entries[i].get()
+
+
+class Home(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.parent = parent
+
+        # Create bracket button
+        Label(self, text="Bracket City", font=36).grid()
+        self.create = Button(self, text="Create Tournament")
+        self.create.bind("<Button-1>", self.create_tourney)
+        self.create.grid()
+
+        # Load bracket button
+        self.load = Button(self, text="Load Tournament")
+        self.load.bind("<Button-1>", self.load_tourney)
+        self.load.grid()
+
+    def create_tourney(self, event):
+        createTeam = Create_Bracket(parent=self.parent, controller=self.controller)
+        createTeam.grid(row=0, column=0, sticky="nsew")
+        createTeam.tkraise()
+
+    def load_tourney(self, event):
+        loadTeam = Load_Bracket(parent=self.parent, controller=self.controller)
+        loadTeam.grid(row=0, column=0, sticky="nsew")
+        loadTeam.tkraise()
+
+
+class Create_Bracket(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.parent = parent
+
+        Label(self, text="Bracket Name:").grid(sticky="w")
+        self.name = StringVar()
+        entry1 = Entry(self, textvariable=self.name)
+        entry1.grid(row=0, column=1)
+
+        Label(self, text="Number of Teams:").grid(row=1, column=0, sticky="w")
+        self.numTeams = IntVar()
+        entry2 = Entry(self, textvariable=self.numTeams)
+        entry2.grid(row=1, column=1)
+
+        Label(self, text="Number of Seeds:").grid(row=2, column=0, sticky="w")
+        self.numSeeds = IntVar()
+        entry3 = Entry(self, textvariable=self.numSeeds)
+        entry3.grid(row=2, column=1)
+
+        button = Button(self, text="Done")
+        button.bind("<Button-1>", self.done_button)
+        button.grid()
+
+    def done_button(self, event):
+        self.controller.brackets[self.name.get()] = {}
+        self.controller.brackets[self.name.get()]["numTeams"] = self.numTeams
+        self.controller.brackets[self.name.get()]["numSeeds"] = self.numSeeds
+        self.controller.home.tkraise()
+
+
+class Load_Bracket(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.parent = parent
+
 
 
 if __name__ == "__main__":
