f79781ff20a2d1223d1a60a40da61baae4b2aa67
Eric Mier <ericjam@umich.edu>
2017-07-15 11:48:27 -0700

    Program is now functional in its most basic form

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import Tkinter as tk
 import math
 from ttk import *
 import ttk
+import functools
 
 class App(tk.Tk):
 
@@ -60,12 +61,20 @@ class App(tk.Tk):
 
 class Bracket(tk.Frame):
 
-    def __init__(self, parent, controller, cols, type):
+    def __init__(self, parent, controller, numTeams, type, name):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.parent = parent
+        self.name = name
+        self.type = type
 
-        rows = 2 ** (cols - 1)
+        cols = 1
+        rem = numTeams
+        while rem > 1:
+            rem /= 2
+            cols += 1
+
+        rows = numTeams
         self.scrollbar = Scrollbar(self)
         self.canvas = Canvas(self, yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.canvas.yview)
@@ -77,8 +86,14 @@ class Bracket(tk.Frame):
         self.frame.grid(row=0, column=0, sticky='news')
 
         sum = 0
-        self.labels = [""] * rows * 2
-        self.ind = 9
+        self.labels = []
+        for i in range(rows * 2):
+            temp = Label(self.canvas)
+            temp.pack()
+            self.labels.append(temp)
+        ind = numTeams
+        end = (numTeams * 2) - 1
+
         for c in range(cols):
             prev = sum
             if c != 0:
@@ -88,36 +103,64 @@ class Bracket(tk.Frame):
                     self.canvas.create_line(10 + (c * 140), 30 + (30 * r) + 15 * (sum), 10 + (c + 1) * 140,
                                             30 + (30 * r) + 15 * (sum))
                     if type == "view":
-                        self.label = Label(self.canvas, text="Holy dog sacks")
-                        self.label.pack()
-                        self.canvas.create_window(80 + (c * 140), 30 + (30 * r) + 15 * (sum),
-                                                  anchor=S, window=self.label)
-                        self.labels.append(self.label)
-                    elif type == "entry" and c == 0:
-                        self.entity = StringVar()
-                        self.entry = Entry(self.canvas, textvariable=self.entity)
-                        self.entry.pack()
-                        self.controller.entries[self.ind] = self.entity
-                        self.ind += 1
+                        label = Label(self.canvas, text=self.controller.brackets[name]["entries"][ind].get())
+                        label.pack()
                         self.canvas.create_window(80 + (c * 140), 25 + (30 * r) + 15 * (sum),
-                                                  anchor=S, window=self.entry)
+                                                  anchor=S, window=label)
+                        if ind >= end:
+                            end = ((ind + 1) / 2) - 1
+                            ind = (ind + 1) / 4
+                            ind -= 1
+                        ind += 1
+
+                    elif type == "entry" and c == 0:
+                        entry = Entry(self.canvas, textvariable=self.controller.brackets[name]["entries"][ind])
+                        entry.pack()
+                        ind += 1
+                        self.canvas.create_window(80 + (c * 140), 25 + (30 * r) + 15 * (sum),
+                                                  anchor=S, window=entry)
+
+                    elif type == "edit":
+                        self.labels[ind] = Label(self.canvas, text=self.controller.brackets[name]["entries"][ind].get())
+                        self.labels[ind].bind("<Button-1>", functools.partial(self.advance, ind=ind))
+                        self.labels[ind].pack()
+                        self.canvas.create_window(80 + (c * 140), 25 + (30 * r) + 15 * (sum),
+                                                  anchor=S, window=self.labels[ind])
+                        if ind >= end:
+                            end = ((ind + 1) / 2) - 1
+                            ind = (ind + 1) / 4
+                            ind -= 1
+                        ind += 1
+
 
                 if (r % 2 ** (c + 1) == 0) and (c < cols - 1):
                     self.canvas.create_line(10 + (140 * (c + 1)), 30 + (30 * r) + 15 * (sum), 10 + (140 * (c + 1)),
                                             30 + (30 * r) + 15 * (sum + (2 ** (c + 1))))
 
 
-        self.button = Button(self.canvas, text="Submit")
-        self.button.bind("<Button-1>", self.submit_entries)
-        self.button.pack()
-        self.canvas.create_window(800, 15, window=self.button)
+        if type == "edit" or type == "entry":
+            button = Button(self.canvas, text="Submit")
+            button.bind("<Button-1>", self.submit_entries)
+            button.pack()
+            self.canvas.create_window(800, 15, window=button)
+
+
         self.update()
         self.canvas.config(scrollregion=self.canvas.bbox("all"))
 
     def submit_entries(self, event):
-        for i in range(len(self.controller.entries)):
-            if self.controller.entries[i].get():
-                print self.controller.entries[i].get()
+        if self.type == "entry":
+            for i in range(len(self.labels)):
+                self.controller.brackets[self.name]["actual"][i] = self.controller.brackets[self.name]["entries"][i]
+
+        bhome = Bracket_Home(parent=self.parent, controller=self.controller, name=self.name)
+        bhome.grid(row=0, column=0, sticky="nsew")
+        bhome.tkraise()
+
+    def advance(self, event, ind):
+        if ind != 1:
+            self.labels[int(math.floor(ind / 2))]["text"] = self.labels[ind]["text"]
+            self.controller.brackets[self.name]["entries"][int(math.floor(ind / 2))] = StringVar(value=self.labels[ind]["text"])
 
 
 class Home(tk.Frame):
@@ -139,7 +182,7 @@ class Home(tk.Frame):
         self.load.grid()
 
     def create_tourney(self, event):
-        createTeam = Create_Bracket(parent=self.parent, controller=self.controller)
+        createTeam = Create_Tournament(parent=self.parent, controller=self.controller)
         createTeam.grid(row=0, column=0, sticky="nsew")
         createTeam.tkraise()
 
@@ -149,7 +192,7 @@ class Home(tk.Frame):
         loadTeam.tkraise()
 
 
-class Create_Bracket(tk.Frame):
+class Create_Tournament(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -161,14 +204,15 @@ class Create_Bracket(tk.Frame):
         entry1 = Entry(self, textvariable=self.name)
         entry1.grid(row=0, column=1)
 
-        Label(self, text="Number of Teams:").grid(row=1, column=0, sticky="w")
+        Label(self, text="Draw size:").grid(row=1, column=0, sticky="w")
         self.numTeams = IntVar()
-        entry2 = Entry(self, textvariable=self.numTeams)
+        entry2 = Combobox(self, textvariable=self.numTeams)
+        entry2['values'] = [2,4,8,16,32,64,128]
         entry2.grid(row=1, column=1)
 
         Label(self, text="Number of Seeds:").grid(row=2, column=0, sticky="w")
         self.numSeeds = IntVar()
-        entry3 = Entry(self, textvariable=self.numSeeds)
+        entry3 = Combobox(self, textvariable=self.numSeeds)
         entry3.grid(row=2, column=1)
 
         button = Button(self, text="Done")
@@ -179,6 +223,14 @@ class Create_Bracket(tk.Frame):
         self.controller.brackets[self.name.get()] = {}
         self.controller.brackets[self.name.get()]["numTeams"] = self.numTeams
         self.controller.brackets[self.name.get()]["numSeeds"] = self.numSeeds
+        self.controller.brackets[self.name.get()]["entries"] = []
+        for i in range(2 * self.numTeams.get()):
+            self.controller.brackets[self.name.get()]["entries"].append(StringVar(value=""))
+
+        self.controller.brackets[self.name.get()]["actual"] = []
+        for i in range(2 * self.numTeams.get()):
+            self.controller.brackets[self.name.get()]["actual"].append(StringVar(value=""))
+
         self.controller.home.tkraise()
 
 
@@ -188,6 +240,63 @@ class Load_Bracket(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.parent = parent
+
+        self.team = StringVar()
+        self.box = Combobox(self, textvariable=self.team)
+        self.box.bind("<<ComboboxSelected>>", self.select)
+        teams = []
+        for key in self.controller.brackets:
+            teams.append(key)
+
+        self.box['values'] = teams
+        self.box.grid()
+
+    def select(self, event):
+        bhome = Bracket_Home(parent=self.parent, controller=self.controller, name=self.team.get())
+        bhome.grid(row=0, column=0, sticky="nsew")
+        bhome.tkraise()
+
+
+class Bracket_Home(tk.Frame):
+
+    def __init__(self, parent, controller, name):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.parent = parent
+        self.name = name
+        self.numTeams = self.controller.brackets[name]["numTeams"]
+        self.numSeeds = self.controller.brackets[name]["numSeeds"]
+        self.entries = [StringVar()] * (self.numTeams.get() * 2)
+        self.actual = [StringVar()] * (self.numTeams.get() * 2)
+
+        button1 = Button(self, text="Create/Edit Original Draw")
+        button1.bind("<Button-1>", self.create_button)
+        button1.grid()
+
+        button2 = Button(self, text="Make Picks")
+        button2.bind("<Button-1>", self.make_button)
+        button2.grid()
+
+        button3 = Button(self, text="View Picks")
+        button3.bind("<Button-1>", self.view_button)
+        button3.grid()
+
+    def create_button(self, event):
+        self.create = Bracket(parent=self.parent, controller=self.controller, numTeams=self.numTeams.get() , type="entry",
+                              name=self.name)
+        self.create.grid(row=0, column=0, sticky="nsew")
+        self.create.tkraise()
+    def make_button(self, event):
+        self.make = Bracket(parent=self.parent, controller=self.controller, numTeams=self.numTeams.get() , type="edit",
+                            name=self.name)
+        self.make.grid(row=0, column=0, sticky="nsew")
+        self.make.tkraise()
+
+    def view_button(self, event):
+        self.view = Bracket(parent=self.parent, controller=self.controller, numTeams=self.numTeams.get(), type="view",
+                            name=self.name)
+        self.view.grid(row=0, column=0, sticky="nsew")
+        self.view.tkraise()
 
 
 
---
+++ practice.py
@@ -0,0 +1,4 @@
+var = "a"
+list = [var] * 5
+list[2] = "b"
+print list
